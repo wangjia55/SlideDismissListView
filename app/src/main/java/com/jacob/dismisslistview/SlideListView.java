37fbc090343ae59b7a6ef7a6cd4f0b53bec3e04f
jacob-wj <wangjia199055@126.com>
2015-04-23 09:39:25 +0800

    Initial commit

---
+++ app/src/main/java/com/jacob/dismisslistview/SlideListView.java
@@ -0,0 +1,26 @@
+package com.jacob.dismisslistview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by jacob-wj on 2015/4/23.
+ */
+public class SlideListView extends ListView {
+
+    public SlideListView(Context context) {
+        super(context);
+    }
+
+    public SlideListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SlideListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
